6a251954035c87d4b864fd7325f2e90a08480264
bethon <bethonxyfu@tencent.com>
2018-12-13 10:21:52 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { yan, B } from './test2';
+
+import yan2 from './test2';
+
+console.log(yan);
+console.log(yan2);
+
+var a = 1 + 1;
+var b = a;
+console.log(a);
+console.log(b);
+console.log(B);
+
+export function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+export const name = 'base';
+
